549265868f3f894582bb93bc7b3f40daaa93e1b1
ZHUANGHAOCHENG <1604088003@qq.com>
2019-07-02 13:19:04 +0800

    Initial Commit

---
+++ Script/SaberCut.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 剑切开方块的脚本
+/// </summary>
+public class SaberCut : MonoBehaviour {
+
+    public Material capMaterial;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        //当Collision/Rigidbody触发另一个时调用
+        GameObject victim = other.collider.gameObject;
+
+        GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+        if (!pieces[1].GetComponent<Rigidbody>())
+            pieces[1].AddComponent<Rigidbody>();
+
+        Destroy(pieces[1], 1);
+    }
+}
