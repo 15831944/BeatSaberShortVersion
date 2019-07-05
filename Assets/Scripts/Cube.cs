0d3ef1d9fa5be5172ca3bdab13c9817082ef67be
ZHUANGHAOCHENG <1604088003@qq.com>
2019-07-05 13:01:08 +0800

    完善了移动速度的计算方式，增加了一个存放全局变量的命名空间

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Cube : MonoBehaviour {
-    private float cameraZPosition = -3.45f;//摄像机的Z坐标，没有找到特别好的办法直接获取摄像机的Z坐标，获取Z坐标是在Z轴垂直屏幕的前提下
-    public float speed = 4.73f;//v=s/t,s就是方块产生的XY平面距离摄像机所在XY平面的距离，t是根据歌曲的BPM算出的多少秒一拍，目前是使用了1/2的BPM，不然速度会很快
-	// Use this for initialization
+    private float cameraZPosition = CreateCube.cameraZPosition;//摄像机的Z坐标，没有找到特别好的办法直接获取摄像机的Z坐标，获取Z坐标是在Z轴垂直屏幕的前提下
+    /// <summary>
+    /// 物体运动的速度，V = S/T；s就是方块产生的XY平面距离摄像机所在XY平面的距离，t是根据歌曲的BPM算出的多少秒一拍，目前是使用了1/2的BPM
+    /// </summary>
+    public static float speed = 1f;
+    public GLOBAL_PARA.CubePara.TypeOfCube TypeOfCube;
+    public GLOBAL_PARA.CubePara.HeatPoint HeatPoint;
 	void Start () {
+        speed = (CreateCube.cameraZPosition - CreateCube.cubeZPosition) / CreateCube.halfBeat;
     }
-	
-	// Update is called once per frame
 	void Update () {
         //判断物体是不是过了视界了，过了就销毁
         if (this.gameObject.transform.position.z <= cameraZPosition)
@@ -17,6 +20,6 @@ public class Cube : MonoBehaviour {
             Destroy(gameObject);
         }
         //让物体进行移动，摄像机在后面，前面要加个负号
-        transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
 	}
 }
---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GLOBAL_PARA
+{
+    public class Global
+    {
+        
+    }
+    public class Game
+    {
+        /// <summary>
+        /// CubeHeatRecord记录被正确击中的物体的数量
+        /// </summary>
+        public static int CubeHeatRecord = 0;
+        /// <summary>
+        /// CubeSendRecord记录一共产生了多少个实体
+        /// </summary>
+        public static int CubeSendRecord = 0;
+        /// <summary>
+        /// 清空记录板
+        /// </summary>
+        public static void ClearRecord()
+        {
+            CubeHeatRecord = 0;
+            CubeSendRecord = 0;
+        }
+        /// <summary>
+        /// 返回击中方块的百分比
+        /// </summary>
+        /// <returns>返回击中的方块数占总的方块数的比例</returns>
+        public float GetHeatPercent()
+        {
+            return CubeHeatRecord / CubeSendRecord;
+        }
+    }
+    public class CubePara
+    {
+        /// <summary>
+        /// 枚举物体的类型，一共有RED和BLUE对应左右手的光剑
+        /// </summary>
+        public enum TypeOfCube
+        {
+            RED = 0,
+            BULE = 1,
+        };
+        /// <summary>
+        /// 要求击中物体的方向，上下左右以及任意点
+        /// </summary>
+        public enum HeatPoint
+        {
+            UP = 0,
+            DOWN = 1,
+            LEFT = 2,
+            RIGHT = 3,
+            ANY = 4,
+        };
+    }
+    public class SaberPara
+    {
+        /// <summary>
+        /// 枚举光剑的类型，一共有RED和BLUE两种
+        /// </summary>
+        public enum TypeOfSaber
+        {
+            RED = 0,
+            BLUE = 1,
+        };
+    }
+
+}
+
