a8123b73eeb23482864f6768b276f60cc567779b
王翀 <974402134@qq.com>
2019-07-10 11:12:39 +0800

    优化光剑的切割方法

--- Assets/Scripts/CreateCube.cs
+++ Assets/Scripts/CreateCube.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CreateCube : MonoBehaviour {
-    public static float cameraZPosition = 0f;//存放摄像机位置的Z坐标，用于配合计算速度
+    public static float cameraZPosition = 1f;//存放摄像机位置的Z坐标，用于配合计算速度
     public static float cubeZPosition  = 3f;//存放方块生成的位置的Z坐标，用于配合计算速度
     private float timerOne = 0f;//计时器
     /// <summary>
     /// 歌曲的BPM的1/2，可以根据不同的难度设置不同的比值
     /// </summary>
     public static float halfBeat = 1.36f;
-    public AudioSource theAudio ;
+    //public AudioSource theAudio ;
     public GameObject cube;
 	void Start () {
-        theAudio = GetComponent<AudioSource>();
-        theAudio.Play();
+        //theAudio = GetComponent<AudioSource>();
+        //theAudio.Play();
         //确定摄像机的位置
         CreateCube.cameraZPosition = this.gameObject.transform.position.z;
+        Debug.Log("Z position: " + this.gameObject.transform.position.z);
         //初始化生成一个物体
-        Instantiate(cube, new Vector3(Random.Range(-1.5f, -1f), Random.Range(0.5f, 1.5f), cubeZPosition), Quaternion.identity);
+        Instantiate(cube, new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(0.3f, 0.9f), cubeZPosition), Quaternion.identity);
+        Debug.Log("INit OVER");
         GLOBAL_PARA.Game.ClearRecord();
         GLOBAL_PARA.Game.CubeSendRecord += 1;
     }
@@ -28,11 +30,11 @@ public class CreateCube : MonoBehaviour {
         //deltaTime是两帧之间的间隔时间，累计超过一定的时间后产生新的方块
         timerOne += Time.deltaTime;
         //音乐结束后的处理，当作游戏结束的标志。
-        if (!theAudio.isPlaying)
-        {
-            Debug.Log("游戏结束");
-            //SceneManager.LoadScene("GameEnd");
-        }
+        //if (!theAudio.isPlaying)
+        //{
+        //    Debug.Log("游戏结束");
+        //    //SceneManager.LoadScene("GameEnd");
+        //}
         if (timerOne > halfBeat)
         {
             Instantiate(cube, new Vector3(Random.Range(0f, 2f), Random.Range(0f, 1f), cubeZPosition), Quaternion.identity);
--- Assets/Scripts/SaberCut.cs
+++ Assets/Scripts/SaberCut.cs
@@ -14,17 +14,21 @@ public class SaberCut : MonoBehaviour {
 
     }
 
-    void OnCollisionEnter(Collision other)
+    void OnCollisionEnter(Collision col)
     {
         //当Collision/Rigidbody触发另一个时调用
-        GameObject victim = other.collider.gameObject;
+        GameObject victim = col.collider.gameObject;
 
-        GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, new Vector3( transform.right.x , transform.right.y , 0 ), capMaterial);
+        GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, col.contacts[0].point, new Vector3( col.contacts[0].normal.y, col.contacts[0].normal.x, 0), capMaterial);
 
         if (!pieces[1].GetComponent<Rigidbody>())
             pieces[1].AddComponent<Rigidbody>();
 
+        pieces[0].GetComponent<Collider>().enabled = false;
+
         Destroy(pieces[0], 1);
         Destroy(pieces[1], 1);
+
+        Instantiate(victim, victim.transform.position, Quaternion.identity);
     }
 }
