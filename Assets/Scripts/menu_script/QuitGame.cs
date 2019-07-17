9e183f83f6026f3357b9c0f316359576327b91e6
QiuyuanLi88 <49109719+QiuyuanLi88@users.noreply.github.com>
2019-07-18 00:15:04 +0800

    增加菜单脚本

---
+++ Assets/Scripts/menu_script/QuitGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;//用于退出运行
+        #else
+            Application.Quit();
+        #endif
+    }
+}
---
+++ Assets/Scripts/menu_script/SwitchToSence.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchToSence : MonoBehaviour {
+    private static string bgmName;  //为当前选中音乐的名称
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //切换页面
+    public void ToPlaySence()
+    {
+        bgmManager.instance.stopbgm();
+        bgmName = bgmManager.instance.getcurrentAudioName();
+        //SampleScene为需要转换至的场景的名称
+        SceneManager.LoadScene("Back_CubeDistance");
+    }
+
+
+}
