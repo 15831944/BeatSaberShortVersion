ed44409c0ff4ae41091ab8947d9264d78305b298
王翀 <974402134@qq.com>
2019-07-18 15:55:27 +0800

    替换切割函数，增加溶解特效，增加火花特效

--- Assets/Scripts/Saber.cs
+++ Assets/Scripts/Saber.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
+using EzySlice;
 
 /// <summary>
 /// 剑切开方块的脚本
@@ -21,12 +22,27 @@ public class Saber : MonoBehaviour {
     /// <summary>
     /// 切割面的材质
     /// </summary>
-    public Material capMaterial;
+    public Material sectionMaterial;
 
     /// <summary>
     /// 剑的末端，用于判断切割方向(解决了平移与旋转）
     /// </summary>
     public GameObject endPoint;
+
+    /// <summary>
+    /// 剑的起始端，用于发射射线
+    /// </summary>
+    public GameObject startPoint;
+
+    /// <summary>
+    /// 与另一把剑碰撞的火花特效
+    /// </summary>
+    public GameObject saberSpark;
+
+    /// <summary>
+    /// 切割方块的火花特效
+    /// </summary>
+    public GameObject cutSpark;
 
     /// <summary>
     /// 剑的颜色属性
@@ -45,12 +61,32 @@ public class Saber : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-
+        saberSpark.SetActive(false);
     }
 
     private void Update()
     {
+        //记录末端上一帧位置信息
         lastPosition = endPoint.transform.position;
+
+        //判断是否碰撞到另一把光剑
+        Vector3 startPosition = startPoint.transform.position;
+        Vector3 endPosition = endPoint.transform.position;
+        Vector3 saberDirection = endPosition - startPosition;
+        RaycastHit hit;
+
+        if(Physics.Raycast(startPosition, saberDirection,out hit, saberDirection.magnitude))
+        {
+            GameObject victim = hit.collider.gameObject;
+
+            if (victim.GetComponent<Saber>())
+            {
+                saberSpark.SetActive(true);
+                saberSpark.transform.position = hit.point;
+                return;
+            }
+        }
+        saberSpark.SetActive(false);
     }
 
     void OnCollisionEnter(Collision col)
@@ -96,37 +132,63 @@ public class Saber : MonoBehaviour {
         //根据末端点的上一帧位置计算出切割方向
         Vector3 direction = endPoint.transform.position - lastPosition;
         //优化切割，避免切割边角
-        Vector3 cutPoint = (cube.transform.position + collision.contacts[0].point) / 2;
+        Vector3 centerPoint = cube.transform.position;
+        Vector3 colPoint = collision.contacts[0].point;
+        Vector3 cutPoint = (centerPoint + colPoint) / 2;
 
-        GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(cube, cutPoint, new Vector3(direction.y, -direction.x, 0), new Material(capMaterial));
+        //调用切割方块的的函数
+        GameObject[] pieces = cube.SliceInstantiate(cutPoint, new Vector3(direction.y, -direction.x, 0), sectionMaterial);
+        Destroy(cube);
 
-        if (!pieces[1].GetComponent<Rigidbody>())
-            pieces[1].AddComponent<Rigidbody>();
-        pieces[0].GetComponent<Rigidbody>().useGravity = true;
+        //添加刚体与溶解特效
+        pieces[0].AddComponent<Rigidbody>();
+        pieces[1].AddComponent<Rigidbody>();
+        pieces[0].AddComponent<CubeDissolve>();
+        pieces[1].AddComponent<CubeDissolve>();
 
-        switch (GetHitFrom(cube.transform.position, collision.contacts[0].point))
+        #region  优化弹开效果，并根据方向生成火花
+        float angleX = Vector2.SignedAngle(Vector2.right, new Vector2(direction.x, direction.y));
+        Vector3 sparkPosition = Vector3.zero;
+        switch (GetHitFrom(centerPoint, colPoint))
         {
-            case GLOBAL_PARA.HitPoint.RIGHT:
-                pieces[1].GetComponent<Rigidbody>().AddForce(new Vector3(0, 5f) ,ForceMode.Impulse);
-                break;
-
             case GLOBAL_PARA.HitPoint.LEFT:
-                pieces[0].GetComponent<Rigidbody>().AddForce(new Vector3(0, 5f), ForceMode.Impulse);
+                //上侧方块向上弹开
+                pieces[1].GetComponent<Rigidbody>().AddForce(new Vector3(0, 5f), ForceMode.Impulse);
+                //找到切割面的中心点
+                sparkPosition = new Vector3(centerPoint.x, colPoint.y + (centerPoint.x - colPoint.x) * direction.y / direction.x, centerPoint.z);
                 break;
 
-            case GLOBAL_PARA.HitPoint.UP:
-                pieces[0].GetComponent<Rigidbody>().AddForce(new Vector3(3f, 0), ForceMode.Impulse);
-                pieces[1].GetComponent<Rigidbody>().AddForce(new Vector3(-3f, 0), ForceMode.Impulse);
+            case GLOBAL_PARA.HitPoint.RIGHT:
+                //上侧方块向上弹开
+                pieces[0].GetComponent<Rigidbody>().AddForce(new Vector3(0, 5f), ForceMode.Impulse);
+                //找到切割面的中心点
+                sparkPosition = new Vector3(centerPoint.x, colPoint.y + (centerPoint.x - colPoint.x) * direction.y / direction.x, centerPoint.z);
+
                 break;
 
             case GLOBAL_PARA.HitPoint.DOWN:
+                //右侧方块向右弹开
+                pieces[0].GetComponent<Rigidbody>().AddForce(new Vector3(3f, 0), ForceMode.Impulse);
+                //左侧方块向左弹开
+                pieces[1].GetComponent<Rigidbody>().AddForce(new Vector3(-3f, 0), ForceMode.Impulse);
+                //找到切割面的中心点
+                sparkPosition = new Vector3(colPoint.x + (centerPoint.y - colPoint.y) * direction.x / direction.y, centerPoint.y,centerPoint.z);
+
+                break;
+
+            case GLOBAL_PARA.HitPoint.UP:
+                //左侧方块向左弹开
                 pieces[0].GetComponent<Rigidbody>().AddForce(new Vector3(-3f, 0), ForceMode.Impulse);
+                //右侧方块向右弹开
                 pieces[1].GetComponent<Rigidbody>().AddForce(new Vector3(3f, 0), ForceMode.Impulse);
+                //找到切割面的中心点
+                sparkPosition = new Vector3(colPoint.x + (centerPoint.y - colPoint.y) * direction.x / direction.y, centerPoint.y, centerPoint.z);
+
                 break;
         }
+        Destroy(Instantiate(cutSpark, sparkPosition, Quaternion.Euler(new Vector3(90 - angleX, 90, 0))), 0.5f);
+        #endregion
 
-        pieces[0].GetComponent<CubeDissolve>().enabled = true;
-        pieces[1].AddComponent<CubeDissolve>();
     }
 
     /// <summary>
